a838631343e9b3b99a514f005d01d074d7d03d80
hannahnoelle797 <hannahnoelle797@gmail.com>
2019-10-17 17:16:33 -0400

    Final working version. Can enter multiple files

--- Demo.java
+++ Demo.java
@@ -2,10 +2,9 @@ import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Demo2 {
+public class Demo {
 
     static FileWriter fw;
     static int lineNum = 0;
@@ -13,70 +12,74 @@ public class Demo2 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        MyScanner2 ms = new MyScanner2();
+        MyScanner ms = new MyScanner();
         String[] l;
         String[] newInt;
+        String tryagain = "yes";
 
-        System.out.print("Enter name of Julia file with .txt extension: ");
-        String filename = kb.nextLine();
-        try {
-            File file = new File(filename);
-
-            System.out.print("Enter name of output file with .txt extension: ");
-            String output = kb.nextLine();
-            Scanner fileRead = new Scanner(file);
+        do {
+            lineNum = 0;
+            System.out.print("Enter name of Julia file with .txt extension: ");
+            String filename = kb.nextLine();
             try {
-                fw = new FileWriter(output);
+                File file = new File(filename);
 
-                while (fileRead.hasNextLine()) {
-                    lineNum++;
-                    String line = fileRead.nextLine();
-                    String[] parts = line.split(" ");
-                    for (int i = 0; i < parts.length; i++) {
-                        if (parts[i].length() > 0) {
-                            if (ms.if_exists(parts[i])) {
-                                String[] currLex = new String[2];
-                                currLex = ms.returnLexeme();
-                                addData(currLex);
-                            } else {
-                                if (isInt(parts[i])) {
-                                    String[] data = new String[2];
-                                    data = ms.setUserVariable(parts[i], "Integer");
-                                    addData(data);
+                System.out.print("Enter name of output file with .txt extension: ");
+                String output = kb.nextLine();
+                Scanner fileRead = new Scanner(file);
+                try {
+                    fw = new FileWriter(output);
+
+                    while (fileRead.hasNextLine()) {
+                        lineNum++;
+                        String line = fileRead.nextLine();
+                        String[] parts = line.split(" ");
+                        for (int i = 0; i < parts.length; i++) {
+                            if (parts[i].length() > 0) {
+                                if (ms.if_exists(parts[i])) {
+                                    String[] currLex = new String[2];
+                                    currLex = ms.returnLexeme();
+                                    addData(currLex);
                                 } else {
-                                    if (parts[i].length() == 1) {
+                                    if (isInt(parts[i])) {
                                         String[] data = new String[2];
-                                        data = ms.setUserVariable(parts[i], "Variable");
+                                        data = ms.setUserVariable(parts[i], "Integer");
                                         addData(data);
                                     } else {
-                                        String part = parts[i];
-                                        int start = 0;
-                                        char first = part.charAt(0);
-                                        String sub = Character.toString(first);
-                                        for (int j = 0; j < part.length(); j++) {
-                                            sub = part.substring(start, (j + 1));
-                                            if (ms.if_exists(sub)) {
-                                                String[] data = new String[2];
-                                                data = ms.returnLexeme();
-                                                addData(data);
-                                                start = j + 1;
-                                            } else {
-                                                String last = Character.toString(sub.charAt(sub.length() - 1));
-                                                if (ms.if_exists(last)) {
-                                                    String newSub = sub.substring(0, sub.length() - 1);
-                                                    if (isInt(newSub)) {
-                                                        newInt = new String[2];
-                                                        newInt = ms.setUserVariable(newSub, "Integer");
-                                                        addData(newInt);
-                                                    } else {
-                                                        l = new String[2];
-                                                        l = ms.setUserVariable(newSub, "Variable");
-                                                        addData(l);
-                                                    }
-                                                    l = new String[2];
-                                                    l = ms.returnLexeme();
-                                                    addData(l);
+                                        if (parts[i].length() == 1) {
+                                            String[] data = new String[2];
+                                            data = ms.setUserVariable(parts[i], "Variable");
+                                            addData(data);
+                                        } else {
+                                            String part = parts[i];
+                                            int start = 0;
+                                            char first = part.charAt(0);
+                                            String sub = Character.toString(first);
+                                            for (int j = 0; j < part.length(); j++) {
+                                                sub = part.substring(start, (j + 1));
+                                                if (ms.if_exists(sub)) {
+                                                    String[] data = new String[2];
+                                                    data = ms.returnLexeme();
+                                                    addData(data);
                                                     start = j + 1;
+                                                } else {
+                                                    String last = Character.toString(sub.charAt(sub.length() - 1));
+                                                    if (ms.if_exists(last)) {
+                                                        String newSub = sub.substring(0, sub.length() - 1);
+                                                        if (isInt(newSub)) {
+                                                            newInt = new String[2];
+                                                            newInt = ms.setUserVariable(newSub, "Integer");
+                                                            addData(newInt);
+                                                        } else {
+                                                            l = new String[2];
+                                                            l = ms.setUserVariable(newSub, "Variable");
+                                                            addData(l);
+                                                        }
+                                                        l = new String[2];
+                                                        l = ms.returnLexeme();
+                                                        addData(l);
+                                                        start = j + 1;
+                                                    }
                                                 }
                                             }
                                         }
@@ -85,15 +88,17 @@ public class Demo2 {
                             }
                         }
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
+
+                fileRead.close();
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-
-            fileRead.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            System.out.print("Run again with new file? (Yes/No) ");
+            tryagain = kb.nextLine();
+        } while (tryagain.equalsIgnoreCase("yes"));
 
         // printArrayList(ms, lexemes);
 
--- MyScanner.java
+++ MyScanner.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 
-public class MyScanner2 {
+public class MyScanner {
 
     // private String[] reserved_words = { "do", "else", "elseif", "end", "false",
     // "for", "function", "if", "print", "return","true","while"};
@@ -10,17 +10,18 @@ public class MyScanner2 {
     // private String[] comparison = { "<", ">", "<=", ">=", "==", "!=" };
     private ArrayList<String> lexemes = new ArrayList<>(Arrays.asList("do", "else", "elseif", "end", "false", "for",
             "function", "if", "print", "return", "true", "while", "(", ")", "{", "}", "[", "]", "\"", "+", "-", "*",
-            "/", "%", "=", "<", ">", "<=", ">=", "==", "!="));
-    private ArrayList<String> tokens = new ArrayList<>(Arrays.asList("do_statement", "else_statement",
-            "else_if_statement", "end_statement", "boolean_false", "for_statement", "function_statment", "if_statement",
-            "print_statement", "return_statement", "boolean_true", "while_statement", "parenthesis_open",
-            "parenthesis_closed", "curly_open", "curly_closed", "square_open", "square_closed", "back_slash",
-            "add_operator", "sub_operator", "mul_operator", "div_operator", "mod_operator", "eq_operator",
-            "lt_operator", "gt_operator", "le_operator", "ge_operator", "eqal_operator", "ne_operator"));
+            "/", "%", "=", "<", ">", "<=", ">=", "==", "!=", "^", ":"));
+    private ArrayList<String> tokens = new ArrayList<>(
+            Arrays.asList("do_statement", "else_statement", "else_if_statement", "end_statement", "boolean_false",
+                    "for_statement", "function_statment", "if_statement", "print_statement", "return_statement",
+                    "boolean_true", "while_statement", "parenthesis_open", "parenthesis_closed", "curly_open",
+                    "curly_closed", "square_open", "square_closed", "back_slash", "add_operator", "sub_operator",
+                    "mul_operator", "div_operator", "mod_operator", "eq_operator", "lt_operator", "gt_operator",
+                    "le_operator", "ge_operator", "eqal_operator", "ne_operator", "exp_operator", "iter_operator"));
 
     String[] lexeme = { "DNE", "DNE" };
 
-    public MyScanner2() {
+    public MyScanner() {
     }
 
     public boolean if_exists(String lex) {
