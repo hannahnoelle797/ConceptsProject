a85e0d78509786a23a114234213156af3cd8aa63
hannahnoelle797 <hannahnoelle797@gmail.com>
2019-10-18 15:52:57 -0400

    Commiting MyScanner.java

--- myScanner.java
+++ myScanner.java
@@ -1,9 +1,9 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MyScanner {
